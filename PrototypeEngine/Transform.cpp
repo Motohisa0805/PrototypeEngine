0be1f3b79280d2456894bdf63166cae43b0b3522
KusumeMotohisa <link080514@gmail.com>
2026-04-02 00:54:39 +0900

    (再)Transformコンポーネント追加(未依存状態)

--- PrototypeEngine/Transform.cpp
+++ PrototypeEngine/Transform.cpp
@@ -1,5 +1,6 @@
 #include "Transform.h"
 #include "SceneManager.h"
+#include "Actor.h"
 
 #include "imgui.h"
 #include "imgui_impl_sdl3.h"
@@ -49,6 +50,104 @@ Transform::Transform(ActorObject* owner)
 
 Transform::~Transform()
 {
+}
+
+void Transform::RotateToNewForward(const Vector3& forward)
+{
+	// Figure out difference between original (unit x) and new
+	float dot = Vector3::Dot(Vector3::UnitZ, forward);
+	float angle = Math::Acos(dot);
+	// Facing down X
+	if (dot > 0.9999f)
+	{
+		SetLocalRotation(Quaternion::Identity);
+	}
+	// Facing down -X
+	else if (dot < -0.9999f)
+	{
+		SetLocalRotation(Quaternion(Vector3::UnitZ, Math::Pi));
+	}
+	else
+	{
+		// Rotate about axis from cross product
+		Vector3 axis = Vector3::Cross(Vector3::UnitZ, forward);
+		axis.Normalize();
+		SetLocalRotation(Quaternion(axis, angle));
+	}
+}
+
+void Transform::LookAt(const Vector3& targetPosition)
+{
+	Vector3 currentPosition = mPosition;
+	Vector3 forward = (targetPosition - currentPosition).Normalized();
+	Vector3 up = Vector3::UnitY;
+
+	Quaternion rot = Quaternion::LookRotation(forward, up);
+	SetLocalRotation(rot);
+
+	mIsDirty = true;
+}
+
+void Transform::SetPosition(const Vector3& pos)
+{
+	//ワールド座標からローカル座標を逆計算してmLocalPositionを更新
+	mLocalPosition = pos;
+	SetDirty();
+	ComputeWorldTransform();
+}
+
+void Transform::SetRotation(const Quaternion& rotation)
+{
+	mLocalRotation = rotation;
+	SetDirty(); // 更新フラグを立てる
+	ComputeWorldTransform();
+}
+
+void Transform::SetScale(Vector3 scale)
+{
+	mLocalScale = scale;
+	SetDirty();
+	ComputeWorldTransform();
+}
+
+void Transform::ComputeWorldTransform()
+{
+	//更新フラグがfalseなら
+	if (!mIsDirty)
+	{
+		return;
+	}
+
+
+	//ローカル座標計算
+	mLocalTransform = Matrix4::CreateScale(mLocalScale);
+	mLocalTransform *= Matrix4::CreateFromQuaternion(mLocalRotation);
+	mLocalTransform *= Matrix4::CreateTranslation(mLocalPosition);
+
+
+	//親がいたら、親のワールド行列を掛ける
+	if (mParentActor)
+	{
+		// 親のワールドトランスフォームが最新であることを保証する必要がある
+		mParentActor->ComputeWorldTransform();
+		mWorldTransform = mLocalTransform * mParentActor->GetWorldTransform();
+	}
+	//いなかったら
+	else
+	{
+		mWorldTransform = mLocalTransform;
+	}
+	mPosition = mWorldTransform.GetTranslation();
+	mRotation = mWorldTransform.GetRotation();
+	mScale = mWorldTransform.GetScale();
+
+	mIsDirty = false;
+
+	// Inform components world transform updated
+	for (auto comp : mOwner->GetComponents())
+	{
+		comp->OnUpdateWorldTransform();
+	}
 }
 
 const Transform* Transform::GetChildActor(Transform* actor)
