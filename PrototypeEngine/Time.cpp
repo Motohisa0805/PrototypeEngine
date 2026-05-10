51259a7589d6abbcec7f5ebc5045e35e1bf8692d
KusumeMotohisa <link080514@gmail.com>
2026-05-11 01:23:59 +0900

    フレームレートの制御処理を修正

--- PrototypeEngine/Time.cpp
+++ PrototypeEngine/Time.cpp
@@ -4,7 +4,7 @@ float Time::mMaxDeltaTime = 0.05f;
 
 float Time::mFrameRate = 0.0f;
 
-Uint64 Time::mMaxTicksCount = TimeLayout::MAX_FPS;
+Uint64 Time::mMaxTicksCount = 0;
 
 float Time::gTimeScale = 1.0f;
 
@@ -22,12 +22,20 @@ void Time::InitializeDeltaTime()
 
 void Time::UpdateDeltaTime()
 {
-    // 60FPS : 16ms = 16,000,000ns
-    // 120FPS : 8.33ms = 8'333'333ns
-    while ((SDL_GetTicksNS() - gTicksCount) < mMaxTicksCount)
-        ;
+	Uint64 targetTicks = gTicksCount + mMaxTicksCount;
+	Uint64 currentTicks = SDL_GetTicksNS();
 
-    Uint64 currentTicks = SDL_GetTicksNS();
+    if (currentTicks < targetTicks) {
+        Uint64 waitTime = targetTicks - currentTicks;
+		
+        if(waitTime > 1'000'000) // 1ms以上待つ必要がある場合はSDL_DelayPreciseを使用
+        {
+            SDL_DelayNS(waitTime - 500'000); // 5ms前に目覚める
+		}
+        // 最後の微調整は精度のためにビジーウェイトで行う
+        while (SDL_GetTicksNS() < targetTicks);
+        currentTicks = SDL_GetTicksNS();
+    }
 
     // unscaledDeltaTimeをまず計算
     gUnscaledDeltaTime = (currentTicks - gTicksCount) / 1'000'000'000.0f;
@@ -42,7 +50,12 @@ void Time::UpdateDeltaTime()
     gDeltaTime = gUnscaledDeltaTime * gTimeScale;
 
     // フレームレートを計算（こっちはunscaledでもscaledでも好み）
-    mFrameRate = 1.0f / gUnscaledDeltaTime;
+    if (gUnscaledDeltaTime > 0.0f) {
+        mFrameRate = 1.0f / gUnscaledDeltaTime;
+    }
+    else {
+		mFrameRate = 9999.0f; // 無限大を避けるため
+    }
 
     // 次のフレーム用に現在の時刻を保存
     gTicksCount = currentTicks;
@@ -56,6 +69,8 @@ void Time::SetMaxDeltaTime(float time)
 
 void Time::SetMaxTicksCount(FrameRateType type)
 {
+    // 60FPS : 16ms = 16,000,000ns
+    // 120FPS : 8.33ms = 8'333'333ns
     switch (type)
     {
     case Frame_60:
